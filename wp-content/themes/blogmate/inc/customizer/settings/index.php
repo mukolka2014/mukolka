3f105ab2eb5b928bfa93afdd5d7c1a2caf10e36c
mukolka2014 <mukolka772@gmail.com>
2024-10-19 17:39:15 +0300

    sdsd

--- wp-content/themes/blogmate/inc/customizer/settings/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once get_stylesheet_directory() . '/inc/customizer/settings/settings-colors.php';
-require_once get_stylesheet_directory() . '/inc/customizer/settings/settings-header.php';
-require_once get_stylesheet_directory() . '/inc/customizer/settings/settings-blog.php';
